d36a1da4046d0459ecbabfd48bf0411397459ac3
Anatoly <geylert@gmail.com>
2020-02-16 04:35:43 +0200

    v 1.1

--- inventoryList/src/main/java/com/talch/inventoryList/rest/InventoryController.java
+++ inventoryList/src/main/java/com/talch/inventoryList/rest/InventoryController.java
@@ -20,7 +20,7 @@ import com.talch.inventoryList.service.InventoryService;
  * @author Talch
  *
  */
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/list")
 
